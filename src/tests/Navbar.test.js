c1e1b080de659c41c18b2a7d0c4f873963b28960
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-08-11 08:58:03 +0100

    Add Navbar.test.js file

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter as Router} from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 
 describe("Snapshot testing", () => {
-    it("Check if Navbar component is rendered", () => {
-        const tree = renderer.create(<Navbar />).toJSON();
-        expect(tree).toMatchSnapshot();
-    })
+  it("Check if Navbar component is rendered", () => {
+    const tree = renderer.create(<Router><Navbar /></Router>).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 })

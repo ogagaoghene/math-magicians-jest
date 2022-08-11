16f07d9f7df56e4cbfe1af1cdf76c926bb570fab
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-08-11 09:52:18 +0100

    Modified App.test.js file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import App from '../App'
 
  it("Check if App component is rendered", () => {
-    const tree = renderer.create(
-    <Router><App /></Router>).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(
+  <Router><App /></Router>).toJSON();
+  expect(tree).toMatchSnapshot();
 })

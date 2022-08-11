89213af98d98a4004caacd02634c30ecbf9b9563
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-08-11 08:58:42 +0100

    Add Quotes.test.js file

--- src/tests/Quotes.test.js
+++ src/tests/Quotes.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Quotes from '../Components/Quotes'
 
 describe("Snapshot testing", () => {
-    it("Check if Quotes component is rendered", () => {
-        const tree = renderer.create(<Quotes />).toJSON();
-        expect(tree).toMatchSnapshot();
-    })
+  it("Check if Quotes component is rendered", () => {
+    const tree = renderer.create(<Quotes />).toJSON();
+    expect(tree).toMatchSnapshot();
+  }); 
+
+  it('renders quote', () => {
+    const { getByText } = render(<Quotes />);
+    expect( screen.getByText('Mathematics is not about numbers, equations, computations, or algorithms. It is about understanding.') ).toBeInTheDocument();
+   });
 })
+
